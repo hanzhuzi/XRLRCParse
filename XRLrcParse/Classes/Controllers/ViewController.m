5899a930677eef9000115b38511d4ffff1453847
apple <1754410821@qq.com>
2016-02-27 13:43:11 +0800

    commit changed
    提交修改

--- XRLrcParse/Classes/Controllers/ViewController.m
+++ XRLrcParse/Classes/Controllers/ViewController.m
@@ -15,12 +15,15 @@
 #import "LrcParseTool.h"
 #import "LrcModel.h"
 
-@interface ViewController ()
+static NSString * const cellID = @"myCellID";
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) NSArray * lrcArray;
 @property (nonatomic, strong) NSTimer * timer;
 @property (nonatomic, assign) double    seccond;
-@property (weak, nonatomic) IBOutlet UILabel *lrcLabel;
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property (weak, nonatomic) IBOutlet UISlider *playCtrolSlider;
 
 @end
 
@@ -78,24 +81,85 @@
         LrcModel * model1 = self.lrcArray[i];
         LrcModel * model2 = self.lrcArray[i+1];
         if (self.seccond >= [model1.timeStr doubleValue] && self.seccond < [model2.timeStr doubleValue]) {
-            self.lrcLabel.text = model1.lrcStr;
-        }else if (self.seccond >= [model2.timeStr doubleValue]) {
-            self.lrcLabel.text = model2.lrcStr;
+            [self.myTableView selectRowAtIndexPath:[NSIndexPath indexPathForRow:i inSection:0] animated:YES scrollPosition:UITableViewScrollPositionMiddle];
         }
     }
     
     self.seccond++;
 }
 
+- (void)setupTableView
+{
+    self.myTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.myTableView.tableFooterView = [[UIView alloc] init];
+}
+
+- (void)setupUI
+{
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.playCtrolSlider setThumbImage:[UIImage imageNamed:@"player-progress-point-h"] forState:UIControlStateNormal];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self setupTableView];
+    [self setupUI];
     [self startTimer];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 
+}
+
+#pragma mark - UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.lrcArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+    cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    cell.textLabel.font = [UIFont systemFontOfSize:15.0];
+    LrcModel * model = self.lrcArray[indexPath.row];
+    cell.textLabel.text = model.lrcStr;
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40.0;
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    // 停止定时器
+    [self stopTimer];
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    // 开启定时器
+    [self startTimer];
 }
 
 @end
